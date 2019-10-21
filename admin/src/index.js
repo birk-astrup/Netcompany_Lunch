cc500da71b30366c3858863670c814efd70be579
Par Joona <parjoona@hotmail.com>
2019-10-21 15:22:25 +0200

    updated to env

--- admin/src/index.js
+++ admin/src/index.js
@@ -5,6 +5,10 @@ import * as serviceWorker from './serviceWorker';
 import GraphQLProvider from './utils/GraphQLProvider';
 import {Auth0Provider} from './utils/react-auth0-spa.js';
 
+import dotenv from 'dotenv';
+
+dotenv.config()
+
 const onRedirectCallback = appState => {
   window.history.replaceState(
     {},
@@ -15,11 +19,13 @@ const onRedirectCallback = appState => {
   );
 };
 
+console.log(process.env)
+
 ReactDOM.render(
   <Auth0Provider
-    domain={'dev-tqekn8lp.eu.auth0.com'}
-    client_id={'lPS6gU9EMCtz3YLypM8JF3N14qxaYWlo'}
-    audience={'https://luncheon/api'}
+    domain={process.env.REACT_APP_DOMAIN}
+    client_id={process.env.REACT_APP_CLIENT_ID}
+    audience={process.env.REACT_APP_AUDIENCE}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
   >
@@ -32,4 +38,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
